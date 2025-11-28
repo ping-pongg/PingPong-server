dfc3f04bed1e4445c7388ed17f32e290a9f8bbd8
김민지 <minzikx@sookmyung.ac.kr>
2025-11-28 12:57:19 +0900

    feat: Member 엔티티 코드 추가 #3

---
+++ src/main/java/pingpong/backend/domain/member/Member.java
@@ -0,0 +1,41 @@
+package pingpong.backend.domain.member;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pingpong.backend.domain.member.dto.MemberRegisterRequest;
+import static java.util.Objects.requireNonNull;
+
+@Entity
+@Getter
+@Builder
+@Table
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @NaturalId
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String nickname;
+
+    public static Member register(MemberRegisterRequest registerRequest, PasswordEncoder passwordEncoder) {
+        Member member = new Member();
+
+        member.email = requireNonNull(registerRequest.email());
+        member.password = requireNonNull(passwordEncoder.encode(registerRequest.password()));
+        member.nickname = registerRequest.nickname();
+
+        return member;
+    }
+}
