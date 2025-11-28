8e56083789ffccca6057de6eeaec4fc32c2c2619
김민지 <minzikx@sookmyung.ac.kr>
2025-11-28 13:14:21 +0900

    feat: Spring Security 설정 및 로그인 필터(JWT 기반 인증) 추가 #3

---
+++ src/main/java/pingpong/backend/global/auth/config/SecurityConfig.java
@@ -0,0 +1,97 @@
+package pingpong.backend.global.auth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pingpong.backend.global.auth.jwt.JwtFilter;
+import pingpong.backend.global.auth.jwt.JwtUtil;
+import pingpong.backend.global.auth.jwt.LoginFilter;
+import pingpong.backend.global.auth.service.AuthService;
+import pingpong.backend.global.redis.RefreshTokenCacheUtil;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] ALLOWED_GET_URLS = {
+            // Swagger
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+
+            // Member
+            "/api/v1/members/*"
+    };
+
+    private static final String[] ALLOWED_POST_URLS = {
+            "/api/v1/auth/login",
+            "/api/v1/members"
+    };
+
+    private final RefreshTokenCacheUtil refreshTokenCacheUtil;
+    private final JwtUtil jwtUtil;
+    private final AuthService authService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(
+            UserDetailsService customUserDetailsService,
+            PasswordEncoder passwordEncoder
+    ) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setHideUserNotFoundExceptions(false);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(DaoAuthenticationProvider provider) {
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           DaoAuthenticationProvider provider,
+                                           AuthenticationManager authManager) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(provider)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(org.springframework.http.HttpMethod.GET,  ALLOWED_GET_URLS).permitAll()
+                        .requestMatchers(org.springframework.http.HttpMethod.POST, ALLOWED_POST_URLS).permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        LoginFilter loginFilter = new LoginFilter(authManager, refreshTokenCacheUtil, jwtUtil, authService);
+        loginFilter.setFilterProcessesUrl("/api/v1/auth/login");
+
+        //JWTFilter 등록
+        http.addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http.addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
