773b3117bbd441411360a8e7998c0f5633e060d5
김민지 <minzikx@sookmyung.ac.kr>
2026-01-20 00:01:16 +0900

    feat: Team Entity 추가 #7

---
+++ src/main/java/pingpong/backend/domain/team/Team.java
@@ -0,0 +1,57 @@
+package pingpong.backend.domain.team;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "notion")
+    private String notion;
+
+    @Column(name = "figma")
+    private String figma;
+
+    @Column(name = "discord")
+    private String discord;
+
+    @Column(name = "swagger")
+    private String swagger;
+
+    @Column(name = "github")
+    private String github;
+
+    @Column(name = "is_updated")
+    private Boolean isUpdated;
+
+    public static Team create(String name, String notion, String figma, String discord,
+                              String swagger, String github) {
+        return Team.builder()
+                .name(name)
+                .notion(notion)
+                .figma(figma)
+                .discord(discord)
+                .swagger(swagger)
+                .github(github)
+                .isUpdated(Boolean.FALSE)
+                .build();
+    }
+
+    public void markUpdated() {
+        this.isUpdated = Boolean.TRUE;
+    }
+}
+
