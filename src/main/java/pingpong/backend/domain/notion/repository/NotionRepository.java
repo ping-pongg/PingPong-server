6cf0e56a6aa6a69d2be9073648ce8d5ed80cca0d
unknown <minjikim@incross.com>
2026-02-12 17:32:15 +0900

    fix: 사용중이지 않은 코드 제거 #22

--- src/main/java/pingpong/backend/domain/notion/repository/NotionRepository.java
+++ src/main/java/pingpong/backend/domain/notion/repository/NotionRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 import pingpong.backend.domain.notion.Notion;
 
 import java.util.Optional;
-import java.util.List;
 
 public interface NotionRepository extends JpaRepository<Notion, Long> {
 
@@ -19,6 +18,4 @@ public interface NotionRepository extends JpaRepository<Notion, Long> {
     @Query("select n from Notion n where n.team.id = :teamId")
     Optional<Notion> findByTeamIdForUpdate(@Param("teamId") Long teamId);
 
-    @Query("select min(n.team.id) from Notion n where n.team.id in :teamIds and n.accessToken is not null and n.accessToken <> ''")
-    Optional<Long> findMinConnectedTeamId(@Param("teamIds") List<Long> teamIds);
 }
