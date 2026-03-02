56b44708318e165e475054f99d86ee8c468c2a58
김민지 <minzikx@sookmyung.ac.kr>
2026-03-02 19:49:37 +0900

    feat: task 도메인 추가 #99

---
+++ src/main/java/pingpong/backend/domain/task/repository/FlowTaskRepository.java
@@ -0,0 +1,15 @@
+package pingpong.backend.domain.task.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pingpong.backend.domain.task.FlowTask;
+
+import java.util.List;
+
+public interface FlowTaskRepository extends JpaRepository<FlowTask, Long> {
+
+    void deleteAllByTaskId(String taskId);
+
+    List<FlowTask> findAllByFlowId(Long flowId);
+
+    List<FlowTask> findAllByTaskId(String taskId);
+}
