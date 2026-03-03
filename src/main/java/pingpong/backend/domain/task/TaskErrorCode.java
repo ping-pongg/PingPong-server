9feca83f11e36be933113c708315c3265f37608a
minzix <minzikx@sookmyung.ac.kr>
2026-03-03 11:26:11 +0900

    feat: task 에러코드 추가 #104

---
+++ src/main/java/pingpong/backend/domain/task/TaskErrorCode.java
@@ -0,0 +1,18 @@
+package pingpong.backend.domain.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import pingpong.backend.global.exception.ApiErrorCode;
+
+@Getter
+@AllArgsConstructor
+public enum TaskErrorCode implements ApiErrorCode {
+
+    TASK_NOT_FOUND("TASK404", "해당 태스크를 찾을 수 없어요.", HttpStatus.NOT_FOUND),
+    FLOW_NOT_FOUND_IN_MAPPING("TASK_FLOW404", "매핑하려는 Flow가 존재하지 않습니다.", HttpStatus.NOT_FOUND);
+
+    private final String errorCode;
+    private final String message;
+    private final HttpStatus status;
+}
