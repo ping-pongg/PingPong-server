b90c5d58c72aefdb1f983a8f86e1f5c0d3b2e082
minzix <minzikx@sookmyung.ac.kr>
2026-03-04 13:26:14 +0900

    feat: endpoint id들로 qa_case 레코드 조회 함수 추가 #121

--- src/main/java/pingpong/backend/domain/qa/repository/QaCaseRepository.java
+++ src/main/java/pingpong/backend/domain/qa/repository/QaCaseRepository.java
@@ -1,5 +1,6 @@
 package pingpong.backend.domain.qa.repository;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import pingpong.backend.domain.qa.QaCase;
 public interface QaCaseRepository extends JpaRepository<QaCase, Long> {
 
 	List<QaCase> findAllByEndpointId(Long endpointId);
+
+	List<QaCase> findAllByEndpointIdIn(Collection<Long> endpointIds);
 }
