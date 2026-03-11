360819547df9f643b8fd885154ef5bd396de2e4b
minzix <minzikx@sookmyung.ac.kr>
2026-03-11 16:44:32 +0900

    feat: INVALID_REDIRECT_URI 에러 코드 추가 #154

--- src/main/java/pingpong/backend/global/auth/AuthErrorCode.java
+++ src/main/java/pingpong/backend/global/auth/AuthErrorCode.java
@@ -10,7 +10,8 @@ import pingpong.backend.global.exception.ApiErrorCode;
 public enum AuthErrorCode implements ApiErrorCode {
 
     REFRESH_TOKEN_INVALID("TOKEN401", "유효하지 않은 Refresh Token 입니다.", HttpStatus.UNAUTHORIZED),
-    INVALID_OAUTH_CODE("OAUTH400", "유효하지 않은 OAuth 코드입니다.", HttpStatus.BAD_REQUEST);
+    INVALID_OAUTH_CODE("OAUTH400", "유효하지 않은 OAuth 코드입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_REDIRECT_URI("OAUTH403", "등록되지 않은 redirect_uri입니다.", HttpStatus.BAD_REQUEST);
 
     private final String errorCode;
     private final String message;
