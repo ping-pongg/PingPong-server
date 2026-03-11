bf64e0ba93256bf2f631845828540c0d1d37a86f
minzix <minzikx@sookmyung.ac.kr>
2026-03-11 17:06:58 +0900

    feat: NOT_TEAM_MEMBER 에러 코드 추가 #154

--- src/main/java/pingpong/backend/global/auth/AuthErrorCode.java
+++ src/main/java/pingpong/backend/global/auth/AuthErrorCode.java
@@ -11,7 +11,8 @@ public enum AuthErrorCode implements ApiErrorCode {
 
     REFRESH_TOKEN_INVALID("TOKEN401", "유효하지 않은 Refresh Token 입니다.", HttpStatus.UNAUTHORIZED),
     INVALID_OAUTH_CODE("OAUTH400", "유효하지 않은 OAuth 코드입니다.", HttpStatus.BAD_REQUEST),
-    INVALID_REDIRECT_URI("OAUTH403", "등록되지 않은 redirect_uri입니다.", HttpStatus.BAD_REQUEST);
+    INVALID_REDIRECT_URI("OAUTH403", "등록되지 않은 redirect_uri입니다.", HttpStatus.BAD_REQUEST),
+    NOT_TEAM_MEMBER("OAUTH405", "해당 팀의 멤버가 아닙니다.", HttpStatus.FORBIDDEN);
 
     private final String errorCode;
     private final String message;
